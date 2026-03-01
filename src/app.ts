385f3d46a07ddc9d9917ac791e613eca4cbb1c24
“Matthew <Mterlinski@rrc.ca>
2026-03-01 14:47:46 -0600

    had to adjust health endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@
 import express from "express";
 
 import eventRoutes from "./api/v1/routes/eventRoutes";
+import healthRoute from "./api/v1/routes/healthRoute";
 
 const app = express();
 app.use(express.json());
 
+app.use("/api/v1", healthRoute);
 app.use("/api/v1/events", eventRoutes);
 
 export default app;
